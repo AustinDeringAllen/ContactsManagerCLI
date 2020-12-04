b6beee3034be90d5e5039f415a797338df64c4b2
Austin Dering-Allen <AustinDeringAllen@gmail.com>
2020-12-03 17:28:25 -0800

    Added function to display menu

--- ContactsManagerCLI/Program.cs
+++ ContactsManagerCLI/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace ContactsManagerCLI
 {
@@ -6,13 +7,23 @@ namespace ContactsManagerCLI
     {
         public static void Main(string[] args)
         {
-            if(Contacts.ReadContacts())
+            if (Contacts.ReadContacts())
             {
-                Contacts.contactList.ForEach(delegate (Contact contact)
+                Contacts.ContactList.ForEach(delegate (Contact contact)
                 {
                     Console.WriteLine("{0} {1}", contact.Name, contact.Number);
                 });
             }
+        }
+
+        public static void DisplayMenu()
+        {
+            Console.WriteLine("1. View all contacts");
+            Console.WriteLine("2. Search contacts");
+            Console.WriteLine("3. Add a new contact");
+            Console.WriteLine("4. Delete a contact");
+            Console.WriteLine("5. Edit a contact");
+            Console.WriteLine("6. Exit");
         }
     }
 }
